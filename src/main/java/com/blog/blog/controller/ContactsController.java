1dfd16f61f4c9716ecd9c6037153457936545ab1
muzafarov <vlad.psu@mail.ru>
2025-01-13 15:47:56 +0500

    Добавил контроллеры для контактов и о нас

--- src/main/java/com/blog/blog/controller/ContactsController.java
+++ src/main/java/com/blog/blog/controller/ContactsController.java
@@ -1,9 +1,11 @@
 package com.blog.blog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class ContactsController {
 
     @GetMapping("/contacts")
